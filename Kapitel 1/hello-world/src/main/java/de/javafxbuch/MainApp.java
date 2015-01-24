f6cf5e87561d5e5ac6bf203ec38bdbff1ca0e96f
eppleton <info@eppleton.de>
2015-01-24 17:16:15 +0100

    Initial commit

---
+++ Kapitel 1/hello-world/src/main/java/de/javafxbuch/MainApp.java
@@ -0,0 +1,33 @@
+package de.javafxbuch;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainApp extends Application {
+
+    private static final Logger log = LoggerFactory.getLogger(MainApp.class);
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello World!");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
