452f5f0e2b279370f642731936e46117735929c5
eppleton <info@eppleton.de>
2015-04-14 14:03:10 +0200

    new demo

---
+++ Kapitel 2/2.2.1/dialog/src/main/java/de/javafxbuch/MainApp.java
@@ -0,0 +1,40 @@
+package de.javafxbuch;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainApp extends Application {
+
+    private static final Logger log = LoggerFactory.getLogger(MainApp.class);
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello World!");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        Stage stage = new Stage();
+        stage.setTitle("Dialog");
+        stage.setScene(new Scene(new StackPane(new Label("Bitte beachten: seit JDK 8 u40 gibt es auch eine Dialogs API!"))));
+        stage.initOwner(primaryStage);
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.show();
+    }
+
+}
