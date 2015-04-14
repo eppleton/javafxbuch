6188f14bc234bbfc7601439e4b75b377b8945b57
eppleton <info@eppleton.de>
2015-04-14 14:07:59 +0200

    added close button for better usability

--- Kapitel 2/2.2.2/hello-world/src/main/java/de/javafxbuch/MainApp.java
+++ Kapitel 2/2.2.2/hello-world/src/main/java/de/javafxbuch/MainApp.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -26,7 +27,9 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         Circle circle = new Circle(100, 100, 100);
         StackPane root = new StackPane();
-        root.getChildren().add(new Button("Hallo"));
+        Button button = new Button("Close");
+        root.getChildren().add(button);
+        button.setOnAction(e-> Platform.exit());
         Scene scene = new Scene(root, 200, 200);
         root.setShape(circle);
         primaryStage.setScene(scene);
