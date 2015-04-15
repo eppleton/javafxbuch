f4137de7789fb21fafe417ac980fa21190f30f2c
eppleton <info@eppleton.de>
2015-04-15 09:54:25 +0200

    new demo controls-menuitem

--- Kapitel 4/4.2.6/controls-contextmenuitem/src/main/java/de/javafxbuch/MainApp.java
+++ Kapitel 4/4.2.6/controls-contextmenuitem/src/main/java/de/javafxbuch/MainApp.java
@@ -1,11 +1,19 @@
 package de.javafxbuch;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.DoubleBinding;
 import javafx.scene.Scene;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.CustomMenuItem;
+import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCombination;
@@ -46,12 +54,36 @@ public class MainApp extends Application {
         openProjectMenuItem.setAccelerator(KeyCombination.valueOf("Ctrl+O"));
         openProjectMenuItem.setGraphic(new ImageView(
                 new Image("de/javafxbuch/project_open.png")));
+        fileMenu.getItems().add(new SeparatorMenuItem());
+        CheckMenuItem showDukeMenuItem = new CheckMenuItem("Duke anzeigen");
+        showDukeMenuItem.setSelected(true);
+        fileMenu.getItems().add(showDukeMenuItem);
+        imageView.visibleProperty().bind(
+                showDukeMenuItem.selectedProperty());
+        ToggleGroup toggleGroup = new ToggleGroup();
+        RadioMenuItem smallDukeMenuItem = new RadioMenuItem(
+                "Kleiner Duke");
+        smallDukeMenuItem.setToggleGroup(toggleGroup);
+        fileMenu.getItems().add(smallDukeMenuItem);
+        RadioMenuItem largeDukeMenuItem = new RadioMenuItem(
+                "Großer Duke");
+        largeDukeMenuItem.setToggleGroup(toggleGroup);
+        fileMenu.getItems().add(largeDukeMenuItem);
+        DoubleBinding scaleBinding = Bindings.createDoubleBinding(
+                () -> smallDukeMenuItem.isSelected() ? 0.5 : 1.0, smallDukeMenuItem.selectedProperty());
+        imageView.scaleXProperty().bind(scaleBinding);
+        imageView.scaleYProperty().bind(scaleBinding);
 
         imageView.setOnMouseClicked(e -> {
             if (e.getButton() == MouseButton.SECONDARY) {
                 contextMenu.show(imageView, e.getScreenX(), e.getScreenY());
             }
         });
+        Slider fadeSlider = new Slider(0, 1, 1);
+        imageView.opacityProperty().bind(fadeSlider.valueProperty());
+        CustomMenuItem customMenuItem = new CustomMenuItem(fadeSlider,
+                false);
+        fileMenu.getItems().add(customMenuItem);
         Scene scene = new Scene(root, 300, 250);
         primaryStage.setTitle("MenuBar Demo");
         primaryStage.setScene(scene);
