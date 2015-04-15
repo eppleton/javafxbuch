155725db13301ce49f2864123b71837b8c49fdde
eppleton <info@eppleton.de>
2015-04-15 19:05:32 +0200

    new demo css-calculator-lcd

---
+++ Kapitel 7/7.4/fxml-calculator/src/main/java/de/javafxbuch/Main.java
@@ -0,0 +1,27 @@
+package de.javafxbuch;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Calculator.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("de/javafxbuch/calculator_2.css");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
