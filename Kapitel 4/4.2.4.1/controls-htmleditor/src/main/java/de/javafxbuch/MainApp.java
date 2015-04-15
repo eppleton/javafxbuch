ab56f93db88379d6ff52967f6031cefd0f1a54d4
eppleton <info@eppleton.de>
2015-04-15 09:18:02 +0200

    neue demo controls-htmleditor

---
+++ Kapitel 4/4.2.4.1/controls-htmleditor/src/main/java/de/javafxbuch/MainApp.java
@@ -0,0 +1,29 @@
+package de.javafxbuch;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        final TextArea textArea = new TextArea();
+        textArea.setPrefRowCount(10);
+        textArea.setPrefColumnCount(20);
+        textArea.setWrapText(true);
+        StackPane root = new StackPane(new Group(textArea));
+        Scene scene = new Scene(root, 300, 250);
+        primaryStage.setTitle("TextArea Demo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
