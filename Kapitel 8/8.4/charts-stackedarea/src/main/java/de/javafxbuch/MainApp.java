0299859aa622514116aa11c163a59445ee63451e
eppleton <info@eppleton.de>
2015-04-15 19:26:53 +0200

    new demo charts-stackedarea

--- Kapitel 8/8.4/charts-stackedarea/src/main/java/de/javafxbuch/MainApp.java
+++ Kapitel 8/8.4/charts-stackedarea/src/main/java/de/javafxbuch/MainApp.java
@@ -3,8 +3,8 @@ package de.javafxbuch;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.StackedAreaChart;
@@ -16,23 +16,50 @@ public class MainApp extends Application {
     public static void main(String[] args) throws Exception {
         launch(args);
     }
-
-    @Override
-    public void start(Stage primaryStage) {
-        NumberAxis xAxis = new NumberAxis("Zeit", 0, 40, 5);
-        NumberAxis yAxis = new NumberAxis("Preis", 0, 80, 10);
-        ObservableList<XYChart.Series<Double, Double>> lineChartData = FXCollections.observableArrayList(
-                new LineChart.Series<Double, Double>("Orakel", FXCollections.observableArrayList(
-                                new XYChart.Data<Double, Double>(0.0, 10.0), new XYChart.Data<Double, Double>(10.0, 34.4), new XYChart.Data<Double, Double>(20.0, 31.9), new XYChart.Data<Double, Double>(30.0, 42.3), new XYChart.Data<Double, Double>(40.0, 57.7)
+    private void init(Stage primaryStage) {
+        Group root = new Group();
+        primaryStage.setScene(new Scene(root));
+        NumberAxis xAxis = new NumberAxis("Zeit", 1981, 1985, 1);
+        NumberAxis yAxis = new NumberAxis("Verkaufte Einheiten", 0, 160, 10);
+        yAxis.setTickLabelFormatter(new NumberAxis.DefaultFormatter(yAxis,null,"k"));
+        ObservableList<XYChart.Series<Integer, Double>> lineChartData = FXCollections.observableArrayList(
+                new LineChart.Series<Integer, Double>("C64", FXCollections.observableArrayList(
+                                new XYChart.Data<Integer, Double>(1981, 0.0),
+                                new XYChart.Data<Integer, Double>(1982, 10.0),
+                                new XYChart.Data<Integer, Double>(1983, 34.4),
+                                new XYChart.Data<Integer, Double>(1984, 31.9),
+                                new XYChart.Data<Integer, Double>(1985, 42.3)
                         )),
-                new LineChart.Series<Double, Double>("Essape",
-                        FXCollections.observableArrayList(new XYChart.Data<Double, Double>(0.0, 57.7), new XYChart.Data<Double, Double>(10.0, 42.3), new XYChart.Data<Double, Double>(20.0, 33.9),
-                                new XYChart.Data<Double, Double>(30.0, 21.7),
-                                new XYChart.Data<Double, Double>(40.0, 17.3)))
+                new LineChart.Series<Integer, Double>("ZX Spectrum", FXCollections.observableArrayList(
+                                new XYChart.Data<Integer, Double>(1981, 0.0),
+                                new XYChart.Data<Integer, Double>(1982, 57.7),
+                                new XYChart.Data<Integer, Double>(1983, 42.3),
+                                new XYChart.Data<Integer, Double>(1984, 33.9),
+                                new XYChart.Data<Integer, Double>(1985, 21.7)
+                        )),
+                new LineChart.Series<Integer, Double>("CPC464", FXCollections.observableArrayList(
+                                new XYChart.Data<Integer, Double>(1981, 0.0),
+                                new XYChart.Data<Integer, Double>(1982, 0.0),
+                                new XYChart.Data<Integer, Double>(1983, 0.0),
+                                new XYChart.Data<Integer, Double>(1984, 20.0),
+                                new XYChart.Data<Integer, Double>(1985, 40.0) 
+                )),
+                new LineChart.Series<Integer, Double>("Andere", FXCollections.observableArrayList(
+                                new XYChart.Data<Integer, Double>(1981, 10.0),
+                                new XYChart.Data<Integer, Double>(1982, 15.0),
+                                new XYChart.Data<Integer, Double>(1983, 13.0),
+                                new XYChart.Data<Integer, Double>(1984, 11.0),
+                                new XYChart.Data<Integer, Double>(1985, 14.0) 
+                ))
         );
         StackedAreaChart chart = new StackedAreaChart(xAxis, yAxis, lineChartData);
-        Scene scene = new Scene(chart);
-        primaryStage.setScene(scene);
+        chart.setTitle("Heimcomputer-Verkaufszahlen in Deutschland");
+        root.getChildren().add(chart);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        init(primaryStage);
         primaryStage.show();
     }
 
