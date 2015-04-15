fbb3ada2d0fd6fdeb76d19cae9785b868c0d6f22
eppleton <info@eppleton.de>
2015-04-15 11:08:17 +0200

    new demo controls-treetableview-simplecell + cleanup

--- Kapitel 4/4.2.9/controls-treetableview-simplecell/src/main/java/de/javafxbuch/MainApp.java
+++ Kapitel 4/4.2.9/controls-treetableview-simplecell/src/main/java/de/javafxbuch/MainApp.java
@@ -2,18 +2,13 @@ package de.javafxbuch;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.cell.TextFieldTreeCell;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import javafx.util.Callback;
+import javafx.util.StringConverter;
 
 public class MainApp extends Application {
 
@@ -26,29 +21,24 @@ public class MainApp extends Application {
         TreeView treeView = new TreeView();
         TreeItem<Color> root = new TreeItem<>(Color.RED);
         treeView.setRoot(root);
-        treeView.setCellFactory(
-                new Callback<TreeView<Color>, TreeCell<Color>>() {
-                    @Override
-                    public TreeCell<Color> call(TreeView<Color> param) {
-                        TreeCell<Color> treeCell = new TreeCell<Color>() {
-                            @Override
-                            protected void updateItem(Color item, boolean empty) {
-                                super.updateItem(item, empty);
-                                if (!empty && item != null) {
-                                    setText(item.toString());
-                                    Rectangle rectangle = new Rectangle(16, 16);
-                                    rectangle.setFill(item);
-                                    setGraphic(rectangle);
-                                } else {
-                                    setText("");
-                                    setGraphic(null);
-                                }
-                            }
-                        };
-                        return treeCell;
-                    }
+        treeView.setCellFactory(TextFieldTreeCell.forTreeView(new StringConverter<Color>() {
+            @Override
+            public String toString(Color object) {
+                return object.toString();
+            }
+
+            @Override
+            public Color fromString(String string) {
+                Color web = Color.BLACK;
+                try {
+                    web = Color.web(string);
+                } catch (Exception e) {
+// im Fehlerfall geben wir Schwarz als Farbe zurueck 
                 }
-        );
+                return web;
+            }
+        }
+        ));
         root.getChildren()
                 .add(new TreeItem<Color>(Color.BLUE));
         StackPane pane = new StackPane(treeView);
