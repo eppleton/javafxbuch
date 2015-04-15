f50b0278d1bc4af39d65056975ee9b0d669bc675
eppleton <info@eppleton.de>
2015-04-15 11:42:56 +0200

    new demo controls-colorpicker

---
+++ Kapitel 4/4.2.10/controls-colorpicker/src/main/java/de/javafxbuch/MainApp.java
@@ -0,0 +1,27 @@
+package de.javafxbuch;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+       ColorPicker colorPicker = new ColorPicker(); colorPicker.setOnAction( e -> System.out.println( "Farbe "
+        +colorPicker.getValue()));
+        StackPane pane = new StackPane(colorPicker);
+        Scene scene = new Scene(pane, 300, 250);
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+}
