d7b4c36951f37c537e2d7a67031cd0e4b8789c82
eppleton <info@eppleton.de>
2015-04-27 09:39:17 +0200

    new demo 13.3

---
+++ Kapitel 13/13.3/webview-js-callback/src/main/java/de/javafxbuch/ValuePrinter.java
@@ -0,0 +1,8 @@
+package de.javafxbuch;
+
+public class ValuePrinter {
+
+    public void print(String name) {
+        System.out.println("value " + name);
+    }
+}
---
+++ Kapitel 13/13.3/webview-js-callback/src/main/java/de/javafxbuch/WebViewForms.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.javafxbuch;
+
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
+import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import netscape.javascript.JSObject;
+
+public class WebViewForms extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        WebView webView = new WebView();
+        final WebEngine engine = webView.getEngine();
+        engine.load(WebViewForms.class
+                .getResource("form.html").toExternalForm());
+        engine.getLoadWorker().stateProperty()
+                .addListener(new ChangeListener<Worker.State>() {
+                    @Override
+                    public void changed(
+                            ObservableValue<? extends Worker.State> observable, Worker.State oldValue, Worker.State newValue) {
+                                JSObject script
+                                = (JSObject) engine.executeScript("window");
+                                script.setMember("formValues",
+                                        new ValuePrinter());
+                            }
+                });
+        Scene scene = new Scene(webView, 600, 250);
+        primaryStage.setTitle("JavaFX WebView Callback Demo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+}
