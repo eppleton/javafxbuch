56ed98d20172c3508f3d94f9ca648d1ce33e5830
Dominik Gruntz <dominik.gruntz@fhnw.ch>
2018-08-03 13:14:21 +0200

    Removes unused imports

--- Kapitel 5/5.10/tweetalot/src/main/java/de/javafxbuch/MainApp.java
+++ Kapitel 5/5.10/tweetalot/src/main/java/de/javafxbuch/MainApp.java
@@ -2,25 +2,16 @@ package de.javafxbuch;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.util.Callback;
-import twitter4j.ResponseList;
-import twitter4j.Status;
-import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
 
 public class MainApp extends Application {
 
@@ -52,7 +43,6 @@ public class MainApp extends Application {
         stage.setScene(new Scene(root, 400, 640));
         stage.setTitle("Tweetalot");
         stage.show();
-
     }
 
 }
